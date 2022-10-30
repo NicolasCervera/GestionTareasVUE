0378f55c94a8c1eaefd4e584cffcf0c660766f43
emiliocervera <emilio.cervera@lmvsoftware.com>
2022-10-30 20:32:20 -0300

    editar y eliminar nuevas tareas

--- frontend-gestion-tareas/src/components/Tarea.vue
+++ frontend-gestion-tareas/src/components/Tarea.vue
@@ -6,19 +6,31 @@
             <div class="card mt-4">
                 <div class="card-body">
                     <div class="input-group">
-                        <input type="text" class="form-control form-control-lg" placeholder="Agregar tarea" v-model="tarea">
+                        <input type="text" class="form-control form-control-lg" placeholder="Agregar tarea"
+                            v-model="tarea">
                         <div class="input-group-append">
                             <button v-on:click="agregarTarea()" class="btn btn-success btn-lg">Agregar</button>
                         </div>
                     </div>
                     <br>
-        
+                    <!--  {{listTareas}} -->
+                    <h5 v-if="listTareas.length == 0">No hay tareas para realizar</h5>
                     <ul class="list-group">
-                        <li v-for="(tarea, index) of listTareas"  :key="index"
-                         class="list-group-item d-flex justify-content-between">
-                            <span class="cursor"><i class="fa-regular fa-circle"></i></span>
-                            {{tarea.nombre}}
-                            <span class="text-danger cursor"><i class="fa-solid fa-trash-can"></i></span>
+                        <li v-for="(tarea, index) of listTareas" :key="index"
+                            class="list-group-item d-flex justify-content-between">
+                            <span class="cursor" v-on:click="editarTarea(tarea, index)" v-bind:class="{'text-success': tarea.estado}">
+                                <!-- Dos maneras de realizar para dos tipos de estados -->
+                                <!-- <i v-if="listTareas[index].estado == false" class="fa-regular fa-circle"></i>
+                                <i v-if="listTareas[index].estado == true" class="fa-solid fa-check"></i> -->
+
+                                <i v-bind:class="[tarea.estado ? 'fa-solid fa-circle-check' : 'fa-regular fa-circle']"></i>
+                            </span>
+                            <p>
+                                {{ tarea.nombre }}
+                            </p>
+                            <span class="text-danger cursor">
+                                <i class="fa-solid fa-trash-can" v-on:click="eliminarTarea(index)"></i>
+                            </span>
                         </li>
                     </ul>
                 </div>
@@ -28,29 +40,35 @@
 </template>
 
 <script>
-    export default {
-        name: 'Tarea',
-        data(){
-            return{
-                tarea: '',
-                listTareas:[]
+export default {
+    name: 'Tarea',
+    data() {
+        return {
+            tarea: '',
+            listTareas: []
+        }
+    },
+    methods: {
+        agregarTarea() {
+            const tarea = {
+                nombre: this.tarea,
+                estado: false
             }
+            this.listTareas.push(tarea);
+            this.tarea = '';
         },
-        methods:{
-            agregarTarea(){
-                const tarea = {
-                    nombre: this.tarea,
-                    estado: false
-                }
-                this.listTareas.push(tarea);
-                this.tarea = '';
-            }
+        eliminarTarea(index) {
+            this.listTareas.splice(index, 1)
+        },
+        editarTarea(tarea, index) {
+            this.listTareas[index].estado = !tarea.estado;
         }
     }
+}
 </script>
 
 <style scoped>
-.cursor{
+.cursor {
     cursor: pointer;
 }
 </style>
